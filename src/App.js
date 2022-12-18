8e039f310ada84cfe24fb4aa38f81f3d80319698
John Roller <jtroller33@gmail.com>
2022-12-17 23:21:55 -0600

    index.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg className="bi me-2" width="40" height="32"><a href="bootstrap"></a></svg>
+        <span className="fs-4">Simple header</span>
+      </a>
+
+      <ul className="nav nav-pills">
+        <li className="nav-item"><a href="https://github.com/jroller33/react-weather-app" className="nav-link active" aria-current="page">Home</a></li>
+        <li className="nav-item"><a href="https://github.com/jroller33/react-weather-app" className="nav-link">Features</a></li>
+        <li className="nav-item"><a href="https://github.com/jroller33/react-weather-app" className="nav-link">Pricing</a></li>
+        <li className="nav-item"><a href="https://github.com/jroller33/react-weather-app" className="nav-link">FAQs</a></li>
+        <li className="nav-item"><a href="https://github.com/jroller33/react-weather-app" className="nav-link">About</a></li>
+      </ul>
+    </header>
     </div>
   );
 }
 
 export default App;
+
+// <header className="App-header">
+// <img src={logo} className="App-logo" alt="logo" />
+// <p>
+//   Edit <code>src/App.js</code> and save to reload.
+// </p>
+// <a
+//   className="App-link"
+//   href="https://reactjs.org"
+//   target="_blank"
+//   rel="noopener noreferrer"
+// >
+//   Learn React
+// </a>
+// </header>

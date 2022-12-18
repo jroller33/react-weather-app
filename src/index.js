4e93d4d655ca79eca83270203b0a6d1a6978c98a
John Roller <jtroller33@gmail.com>
2022-12-17 23:32:59 -0600

    added header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+
+// not sure if index.css and App.css both need to be imported
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
